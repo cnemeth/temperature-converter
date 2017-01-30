31ff516365831e9df967eeb2a5f1676c1e7ef60a
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2017-01-29 18:11:15 -0800

    Added images for signifying hot-cold temperatures. Below 50F/10degC the cold temperature image is shown. Otherwise, the normal image is shown.

--- app/components/temperature-converter.js
+++ app/components/temperature-converter.js
@@ -2,19 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['temperature-converter'],
+  isCold: false,
 
   actions: {
-    convertTemperature: function() {
+    convertTemperature() {
       var cels = this.get('celsius');
       var fahr = this.get('fahrenheit');
 
       if ( !Ember.isEmpty(cels) && !isNaN(cels) ) {
         var f = (9/5 * cels + 32);
+        if (f < 50) {
+          this.set('isCold', true);
+        } else {
+          this.set('isCold', false);
+        }
         this.set('fahrenheit', f.toFixed(1));
       }
 
       if ( !Ember.isEmpty(fahr) && !isNaN(fahr) ) {
         var c = (5/9) * (fahr - 32);
+        if (c < 10) {
+          this.set('isCold', true);
+        } else {
+          this.set('isCold', false);
+        }
         this.set('celsius', c.toFixed(1));
       }
     }
